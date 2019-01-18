620c5ebf377056fd86fe7e4a0705fdd6b318f9ea
tribles <senthilkumar.s.shanmugam@oracle.com>
2019-01-18 18:55:12 +0530

    fixed

--- OAuth2JWT/src/main/java/lab/rest/security/OAuth2JWT/security/SecurityConfig.java
+++ OAuth2JWT/src/main/java/lab/rest/security/OAuth2JWT/security/SecurityConfig.java
@@ -1,37 +1,95 @@
 package lab.rest.security.OAuth2JWT.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenStoreUserApprovalHandler;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 @EnableWebSecurity(debug=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http.
-		     formLogin().disable().
-		     anonymous().disable().
-		     authorizeRequests().anyRequest().denyAll();
-		     
+	@Autowired
+	private ClientDetailsService clientDetailsService;
+	
+	
+	@Autowired
+	protected void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+		.withUser("crmadmin").password("crmpass").roles("ADMIN","USER").and()
+        .withUser("crmuser").password("pass123").roles("USER");
+		      
 	}
 	
+	/*@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(crm)
+	}*/
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
 	
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-		auth.inMemoryAuthentication().
-		      withUser("user").password("password").roles("USER").and()
-		      .withUser("admin").password("admin").authorities("ROLE_ADMIN");
-		      
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    protected void configure(HttpSecurity http) throws Exception {
+		http
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.csrf().disable()
+	  	.authorizeRequests()
+	  	.antMatchers("/about").permitAll() 
+        .antMatchers("/signup").permitAll()
+	  	.antMatchers("/oauth/token").permitAll()
+	    .anyRequest().authenticated()
+	  	.and()
+	  	.httpBasic()
+	  		.realmName("CRM_REALM");
+    }
+
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManagerBean();
+	@Autowired
+	public TokenStoreUserApprovalHandler userApprovalHandler(TokenStore tokenStore){
+		TokenStoreUserApprovalHandler handler = new TokenStoreUserApprovalHandler();
+		handler.setTokenStore(tokenStore);
+		handler.setRequestFactory(new DefaultOAuth2RequestFactory(clientDetailsService));
+		handler.setClientDetailsService(clientDetailsService);
+		return handler;
+	}
+	
+	@Bean
+	@Autowired
+	public ApprovalStore approvalStore(TokenStore tokenStore) throws Exception {
+		TokenApprovalStore store = new TokenApprovalStore();
+		store.setTokenStore(tokenStore);
+		return store;
 	}
 }
